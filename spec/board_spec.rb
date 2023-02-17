e305edf1fbc474546548a6dcc2ecd502b93773eb
Amy Spears <amspears007@gmail.com>
2023-02-17 16:38:15 -0700

    Test: creates more robust testing to ensure board instances are formatted correctly

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Board do
     board = Board.new
     expect(board).to be_instance_of(Board)
   end
+
+  it 'creates cells in a hash upon initiating' do
+    board = Board.new
+    board.cells
+    expect(board.cells).to be_instance_of(Hash)
+    expect(board.cells.keys.length).to eq(16)
+    expect(board.cells.values.length).to eq(16)
+    expect(board.cells['A1']).to be_instance_of(Cell)
+  end
 end
