cd5cc0d5df55401adfc6e2d8291f06428b4af52c
Amy Spears <amspears007@gmail.com>
2023-02-17 16:46:19 -0700

    Feat/Test:validates coordinates tests and method in Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,10 +8,18 @@ RSpec.describe Board do
 
   it 'creates cells in a hash upon initiating' do
     board = Board.new
-    board.cells
     expect(board.cells).to be_instance_of(Hash)
     expect(board.cells.keys.length).to eq(16)
     expect(board.cells.values.length).to eq(16)
     expect(board.cells['A1']).to be_instance_of(Cell)
+  end
+
+  it 'validates coordinates on the board' do
+    board = Board.new
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
   end
 end
