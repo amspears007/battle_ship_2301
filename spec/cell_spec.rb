0a41d4492a7966c20be9cde8e8289be1655d43f4
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-16 18:21:55 -0700

    Test/Feat: initialize cell test and cell method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,3 +1,10 @@
 require 'spec_helper'
 
+RSpec.describe Cell do 
 
+  it 'can initialize' do
+    cell = Cell.new("B4")
+    expect(cell).to be_instance_of(Cell)
+    expect(cell.coordinate).to eq("B4")
+  end
+end
