b4b86a6984b5c5c547ba4ca02352f5dd87e69f79
Angel Byun <abyun22@turing.edu>
2023-02-22 15:20:48 -0700

    Refactor: Add let statements to game_spec.rb to clean up tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,25 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe Game do
+
+let(:game) {Game.new}
+let(:user_board) {Board.new}
+let(:comp_board) {Board.new}
+let(:user_cruiser) {Ship.new('Cruiser',3)}
+let(:user_sub) {Ship.new('Submarine', 2)}
+let(:comp_cruiser) {Ship.new('Cruiser', 3)}
+let(:comp_sub) {Ship.new('Submarine', 2)}
+
     it 'initializes' do
-        game = Game.new
         game.new_game
-        user_board = Board.new
-        comp_board = Board.new
         expect(game).to be_instance_of(Game)
         expect(user_board).to be_instance_of(Board)
         expect(comp_board).to be_instance_of(Board)
     end
 
     it 'allows placement of computer submarine' do
-        game = Game.new
         game.new_game
         game.comp_sub_placement
         expect(game.comp_board.render(true) == game.comp_board.render).to be(false)
     end
 
     it 'allows placement of computer cruiser' do
-        game = Game.new
         game.new_game
         game.comp_cruiser_placement
         expect(game.comp_board.render(true) == game.comp_board.render).to be(false)
@@ -27,7 +31,6 @@ RSpec.describe Game do
 
 
     it 'creates response for ship that gets hit' do
-        game = Game.new
         game.new_game
         comp_board = game.comp_board
         user_cruiser = game.user_cruiser
@@ -35,12 +38,9 @@ RSpec.describe Game do
         user_input = comp_board.cells["A1"].coordinate
         comp_board.place(user_cruiser, ["A1", "A2", "A3"])
 		comp_board.cells["A1"].fire_upon
-        # require 'pry'; binding.pry
-        # expect(game.shot_response_player(user_input)).to eq("Your shot on A1 was a hit!")
     end
 
     it 'creates response for impact that misses the ship' do
-        game = Game.new
         game.new_game
         comp_board = game.comp_board
         user_cruiser = game.user_cruiser
@@ -48,11 +48,9 @@ RSpec.describe Game do
         user_input = comp_board.cells["D4"].coordinate
         comp_board.place(user_cruiser, ["A1", "A2", "A3"])
 		comp_board.cells["D4"].fire_upon
-        # expect(game.shot_response_player(user_input)).to eq("Your shot on D4 was a miss!")
     end
 
     it 'creates response when opponents ship has sunk' do
-        game = Game.new
         game.new_game
         comp_board = game.comp_board
         user_cruiser = game.user_cruiser
@@ -62,18 +60,12 @@ RSpec.describe Game do
 		comp_board.cells["A1"].fire_upon
         comp_board.cells["A2"].fire_upon
         comp_board.cells["A3"].fire_upon
-        # expect(game.shot_response_player(user_input)).to eq("The opponent's ship has sunk!")
     end
 
     it 'new_game clears all info from previous game' do
-        game = Game.new
         game.new_game
         comp_board = game.comp_board
         user_cruiser = game.user_cruiser
         print comp_board.render(true)
-        # comp_board.place(user_cruiser, ["A1", "A2", "A3"])
-        # # require 'pry'; binding.pry
-        # game.new_game
-        # print comp_board.render(true)
     end
 end
